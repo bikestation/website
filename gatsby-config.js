b6023228f4e3b15dbb5986500312c51eee5958dd
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-08-05 11:55:10 +0200

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,8 @@ module.exports = {
           }
         ],
       },
-    }
+    },
+    'gatsby-plugin-offline',
     /* {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -49,6 +50,5 @@ module.exports = {
     }, */
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    'gatsby-plugin-offline`,
   ],
 }
